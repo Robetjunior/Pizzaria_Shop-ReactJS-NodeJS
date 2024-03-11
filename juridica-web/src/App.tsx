ec9181278c1cd4eda5a7252ce92c69107d4a7009
Robetjunior <juniorferreirarob@gmail.com>
2024-03-10 21:21:24 -0300

    criação do header e implementação do theme-toggle

--- juridica-web/src/App.tsx
+++ juridica-web/src/App.tsx
@@ -4,13 +4,16 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Toaster, toast } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | facilita-juridico" />
-      <Toaster richColors closeButton />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | facilita-juridico" />
+        <Toaster richColors closeButton />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
