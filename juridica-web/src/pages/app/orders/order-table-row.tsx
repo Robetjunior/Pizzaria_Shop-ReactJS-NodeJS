4af591c86c23aab9e059a66ccb35affd1ed74195
Robetjunior <juniorferreirarob@gmail.com>
2024-03-11 21:34:28 -0300

    criação do componente para detalhe do pedido

--- juridica-web/src/pages/app/orders/order-table-row.tsx
+++ juridica-web/src/pages/app/orders/order-table-row.tsx
@@ -1,6 +1,8 @@
 import { TableCell, TableRow } from "../../../components/ui/table";
 import { Button } from "../../../components/ui/button";
 import { ArrowRight, Search, X } from "lucide-react";
+import { Dialog, DialogTrigger } from "../../../components/ui/dialog";
+import { OrderDetails } from "./order-details";
 
 // export interface OrderTableRowProps {}
 
@@ -8,10 +10,16 @@ export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant={"outline"} size={"xs"}>
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant={"outline"} size={"xs"}>
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         B23PDAOSKJDO
