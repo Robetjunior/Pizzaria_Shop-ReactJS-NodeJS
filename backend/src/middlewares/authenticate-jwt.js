f56bb3cc504df226afb4a20d1223ba24fb66f5eb
Robetjunior <juniorferreirarob@gmail.com>
2024-03-25 18:06:47 -0300

    atualização no AppLayout para fazer o logout automático

--- backend/src/middlewares/authenticate-jwt.js
+++ backend/src/middlewares/authenticate-jwt.js
@@ -24,6 +24,28 @@ export const authenticateJWT = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    return res.status(403).json({ error: "Invalid token." });
+    // Chama a função onError para lidar com o erro
+    return onError({ code: "UNAUTHORIZED", error, set: { status: 401 } }, res);
+  }
+};
+
+// Função para tratamento de erros personalizados
+const onError = ({ code, error, set }, res) => {
+  switch (code) {
+    case "UNAUTHORIZED":
+      // Aqui você pode definir a resposta de erro de acordo com as suas necessidades
+      // Por exemplo, se o erro for de autenticação não autorizada (401)
+      res.status(set.status).json({ code, message: error.message });
+      break;
+    case "NOT_A_MANAGER":
+      // Tratamento para o caso de não ser um gerente
+      res.status(set.status).json({ code, message: error.message });
+      break;
+    default:
+      // Outros tratamentos de erro podem ser definidos aqui
+      res.status(500).json({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal Server Error",
+      });
   }
 };
--- pizzaria/src/app.tsx
+++ pizzaria/src/app.tsx
@@ -3,7 +3,7 @@ import "./global.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { Toaster, toast } from "sonner";
+import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/react-query";
