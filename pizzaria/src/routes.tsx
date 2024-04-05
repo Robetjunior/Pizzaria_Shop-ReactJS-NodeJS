7068f9b606b676904801dcb02d684438b9bc53a2
Robetjunior <juniorferreirarob@gmail.com>
2024-04-05 16:43:02 -0300

    erros e página 404

--- pizzaria/src/routes.tsx
+++ pizzaria/src/routes.tsx
@@ -7,12 +7,13 @@ import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/sign-up";
 import { Orders } from "./pages/app/orders/orders";
 import { NotFound } from "./pages/404";
+import { Error } from "./pages/error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
@@ -26,5 +27,9 @@ export const router = createBrowserRouter([
       { path: "/sign-in", element: <SignIn /> },
       { path: "/sign-up", element: <SignUp /> },
     ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
